e66b651aff850bc5d067e131486c917f9d0688f2
Isaac Gomes <118241033+Isaac-Gomes-085@users.noreply.github.com>
2024-01-09 17:36:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import './index.html' 
 const input = document.querySelector("#input");
 const btn = document.querySelector("#btn-add");
 const container_ul = document.querySelector('.to-do-list')
@@ -24,18 +23,19 @@ function showTasks() {
     console.log(item, index, all_Tasks);
     li += `    
     <li class="list ${item.conclude && 'done'}">${item.task}
-    <div class="conclude-delete">
-    <i class="bi bi-check-circle-fill" id="conclude" onclick="concludeTask(${index})"></i>
-      <lord-icon
-            id="delete"
-            src="https://cdn.lordicon.com/jmkrnisz.json"
-            trigger="morph"
-            colors="primary:green"
-            state="morph-fill"
-            style="width: 30px; height: 30px; cursor: pointer"
-          onclick="deleteItem(${index})">
-      </lord-icon></li>
-      </div>`;
+      <div class="conclude-delete">
+        <i class="bi bi-check-circle-fill" id="conclude" onclick="concludeTask(${index})"></i>
+          <lord-icon
+                id="delete"
+                src="https://cdn.lordicon.com/jmkrnisz.json"
+                trigger="morph"
+                colors="primary:green"
+                state="morph-fill"
+                style="width: 30px; height: 30px; cursor: pointer"
+              onclick="deleteItem(${index})">
+          </lord-icon>
+        </div>
+      </li>`;
 
       container_ul.innerHTML = li
 
